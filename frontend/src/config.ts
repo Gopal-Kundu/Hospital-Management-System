313c430b1e4874455f7e1c2abeb9cb1f8074d945
Gopal Kundu <kundugopal2005@gmail.com>
2026-07-22 12:54:49 +0530

    backend api added

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,2 @@
-export const baseURL = 'http://localhost:5000/api';
+// export const baseURL = 'http://localhost:5000/api';
+export const baseURL = "https://hospital-management-system-sxw2.vercel.app/api";
